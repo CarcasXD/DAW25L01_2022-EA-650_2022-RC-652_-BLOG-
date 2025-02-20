d1372636ffdaa03e9a5ed85155ec3fd3a9e934b3
CarcasXD <carlossalesianoprro@gmail.com>
2025-02-20 17:39:10 -0600

    Conexion y Contexto

---
+++ L01_2022-EA-650_2022-RC-652/Models/blogContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace L01_2022_EA_650_2022_RC_652.Models
+{
+    public class blogContext:DbContext
+    {
+        public blogContext(DbContextOptions<blogContext> options) : base(options)
+        {
+
+        }
+    }
+}
