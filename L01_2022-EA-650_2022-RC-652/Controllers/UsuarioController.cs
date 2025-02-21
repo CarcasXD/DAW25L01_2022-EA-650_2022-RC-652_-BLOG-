9b42931673cdc0247741c2f7d937e72b9c5488e9
CarcasXD <carlossalesianoprro@gmail.com>
2025-02-20 18:40:28 -0600

    Controlador Usuario Completado

--- L01_2022-EA-650_2022-RC-652/Controllers/UsuarioController.cs
+++ L01_2022-EA-650_2022-RC-652/Controllers/UsuarioController.cs
@@ -44,5 +44,42 @@ namespace L01_2022_EA_650_2022_RC_652.Controllers
                 return BadRequest(ex.Message);
             }
         }
+        [HttpPut]
+        [Route("Modificar/{id}")]
+        public IActionResult ModificarUsuario(int id, [FromBody] usuarios usuarioModificar) 
+        {
+            usuarios? usuarioActual = (from e in _blogContext.usuarios
+                                       where e.usuarioId == id select e).FirstOrDefault();
+            if(usuarioActual == null)
+            { return NotFound(); }
+
+            usuarioActual.nombreUsuario = usuarioModificar.nombreUsuario;
+            usuarioActual.rolId = usuarioModificar.rolId;
+            usuarioActual.clave = usuarioModificar.clave;
+            usuarioActual.nombre = usuarioModificar.nombre;
+            usuarioActual.apellido = usuarioActual.apellido;
+
+            _blogContext.Entry(usuarioActual).State = EntityState.Modified;
+            _blogContext.SaveChanges();
+
+            return Ok(usuarioModificar);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+        public IActionResult eliminarUsuario(int id) 
+        {
+            usuarios? usuarioeliminado = (from e in _blogContext.usuarios
+                                          where e.usuarioId == id
+                                          select e).FirstOrDefault();
+            if (usuarioeliminado == null)  return NotFound(); 
+
+            _blogContext.usuarios.Attach(usuarioeliminado);
+            _blogContext.usuarios.Remove(usuarioeliminado);
+            _blogContext.SaveChanges(true);
+
+            return Ok(usuarioeliminado);
+        }
+
     }
 }
